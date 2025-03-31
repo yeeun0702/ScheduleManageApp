8bcc8830280abbbba07031e96d24caa91f5860fb
yeeun0702 <kimyeeun0702@naver.com>
2025-03-31 11:24:21 +0900

    🎉 [chore] 프로젝트 초기 세팅

---
+++ src/main/java/com/example/schedulemanageapp/ScheduleManageAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.schedulemanageapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleManageAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScheduleManageAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/schedulemanageapp/ScheduleManageAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.schedulemanageapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleManageAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

e1b76683020dd831380da1f7e2a2459eccd477e0
yeeun0702 <kimyeeun0702@naver.com>
2025-04-01 16:37:03 +0900

    ✨ [feat] #5 일정 상세 조회 비지니스 로직 및 컨트롤러 구현

--- src/main/java/com/example/schedulemanageapp/domain/schedule/controller/ScheduleController.java
+++ src/main/java/com/example/schedulemanageapp/domain/schedule/controller/ScheduleController.java
@@ -4,14 +4,12 @@ package com.example.schedulemanageapp.domain.schedule.controller;
 import com.example.schedulemanageapp.common.exception.code.enums.SuccessCode;
 import com.example.schedulemanageapp.common.response.ApiResponseDto;
 import com.example.schedulemanageapp.domain.schedule.dto.request.ScheduleCreateRequestDto;
+import com.example.schedulemanageapp.domain.schedule.dto.response.ScheduleDetailResponseDto;
 import com.example.schedulemanageapp.domain.schedule.service.ScheduleService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/schedules")
@@ -26,4 +24,10 @@ public class ScheduleController {
         return ResponseEntity.status(201)
                 .body(ApiResponseDto.success(SuccessCode.SCHEDULE_CREATE_SUCCESS, "/api/schedules"));
     }
+
+    @GetMapping("/{scheduleId}")
+    public ResponseEntity<ApiResponseDto<ScheduleDetailResponseDto>> getSchedule(@PathVariable final Long scheduleId) {
+        return ResponseEntity.ok(ApiResponseDto.success(SuccessCode.SCHEDULE_READ_SUCCESS, scheduleService.getSchedule(scheduleId), "/api/schedules/"));
+    }
+
 }
--- src/main/java/com/example/schedulemanageapp/domain/schedule/service/ScheduleService.java
+++ src/main/java/com/example/schedulemanageapp/domain/schedule/service/ScheduleService.java
@@ -1,8 +1,10 @@
 package com.example.schedulemanageapp.domain.schedule.service;
 
 import com.example.schedulemanageapp.common.exception.base.CustomException;
+import com.example.schedulemanageapp.common.exception.base.NotFoundException;
 import com.example.schedulemanageapp.common.exception.code.enums.ErrorCode;
 import com.example.schedulemanageapp.domain.schedule.dto.request.ScheduleCreateRequestDto;
+import com.example.schedulemanageapp.domain.schedule.dto.response.ScheduleDetailResponseDto;
 import com.example.schedulemanageapp.domain.schedule.entity.Schedule;
 import com.example.schedulemanageapp.domain.schedule.repository.ScheduleRepository;
 import com.example.schedulemanageapp.domain.users.entity.Users;
@@ -18,6 +20,12 @@ public class ScheduleService {
     private final ScheduleRepository scheduleRepository;
     private final UserRepository userRepository;
 
+
+    /**
+     * 일정 생성
+     * - 유저 ID를 통해 유저 정보를 조회한 후, 할일 제목과 내용을 포함한 Schedule 엔티티를 생성 및 저장
+     * @param scheduleCreateRequestDto 생성 요청 DTO
+     */
     @Transactional
     public void createSchedule(ScheduleCreateRequestDto scheduleCreateRequestDto){
         Users user = userRepository.findById(scheduleCreateRequestDto.userId())

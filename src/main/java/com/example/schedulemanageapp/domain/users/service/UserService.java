6500008c4a9e6b29dd1d144b804746f543826c82
yeeun0702 <kimyeeun0702@naver.com>
2025-04-02 00:49:44 +0900

    💡 [docs] UserService에 주석 추가

--- src/main/java/com/example/schedulemanageapp/domain/users/service/UserService.java
+++ src/main/java/com/example/schedulemanageapp/domain/users/service/UserService.java
@@ -20,6 +20,11 @@ public class UserService {
     private final UserRepository userRepository;
     private final UserServiceHelper userServiceHelper;
 
+    /**
+     * 유저 생성
+     *
+     * @param userCreateRequestDto 사용자 생성 요청 데이터를 담은 DTO
+     */
     @Transactional
     public void createUser(final UserCreateRequestDto userCreateRequestDto){
         Users user = new Users(
@@ -30,12 +35,27 @@ public class UserService {
         userRepository.save(user);
     }
 
+
+    /**
+     * 유저 조회
+     *
+     * @param userId 조회할 사용자 ID
+     * @return UserDetailResponseDto 조회된 사용자 정보
+     */
     @Transactional
     public UserDetailResponseDto getUser(final Long userId){
         Users user = userServiceHelper.findUserOrThrow(userId);
         return UserDetailResponseDto.from(user);
     }
 
+
+    /**
+     * 유저 수정
+     *
+     * @param userId 수정할 사용자 ID
+     * @param userUpdateRequestDto 수정할 사용자 정보가 담긴 DTO
+     * @return UserUpdateResponseDto 수정된 사용자 정보
+     */
     @Transactional
     public UserUpdateResponseDto updateUser(final Long userId, final UserUpdateRequestDto userUpdateRequestDto){
 
@@ -45,6 +65,14 @@ public class UserService {
         return UserUpdateResponseDto.from(user);
     }
 
+
+    /**
+     * 유저 삭제
+     * 사용자가 삭제 요청을 할 때, 비밀번호가 맞는지 확인한 후 삭제 처리.
+     *
+     * @param userId 삭제할 사용자 ID
+     * @param userDeleteRequestDto 사용자 삭제 요청 데이터 (비밀번호 포함)
+     */
     @Transactional
     public void deleteUser(final Long userId, final UserDeleteRequestDto userDeleteRequestDto){
 

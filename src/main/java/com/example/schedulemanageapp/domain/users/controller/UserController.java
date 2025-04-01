e710fdd7a5eeda140a89a9e306f4664e9d4a17a1
yeeun0702 <kimyeeun0702@naver.com>
2025-04-01 21:06:03 +0900

    ✨ [feat] #9 선택 유저 조회 비즈니스 로직 및 컨트롤러 구현

--- src/main/java/com/example/schedulemanageapp/domain/users/controller/UserController.java
+++ src/main/java/com/example/schedulemanageapp/domain/users/controller/UserController.java
@@ -3,14 +3,12 @@ package com.example.schedulemanageapp.domain.users.controller;
 import com.example.schedulemanageapp.common.exception.code.enums.SuccessCode;
 import com.example.schedulemanageapp.common.response.ApiResponseDto;
 import com.example.schedulemanageapp.domain.users.dto.request.UserCreateRequestDto;
+import com.example.schedulemanageapp.domain.users.dto.response.UserDetailResponseDto;
 import com.example.schedulemanageapp.domain.users.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/users")
@@ -26,5 +24,9 @@ public class UserController {
                 .body(ApiResponseDto.success(SuccessCode.USER_CREATE_SUCCESS, "/api/users"));
     }
 
+    @GetMapping("{userId}")
+    public ResponseEntity<ApiResponseDto<UserDetailResponseDto>> getUser(@PathVariable final Long userId){
+        return ResponseEntity.ok(ApiResponseDto.success(SuccessCode.USER_READ_SUCCESS, userService.getUser(userId), "/api/users/"));
+    }
 
 }

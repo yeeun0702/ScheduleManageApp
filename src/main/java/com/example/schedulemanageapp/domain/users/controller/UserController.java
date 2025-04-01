e5bcbc37e414ef686ed8ce65cb658babb0e6dd7c
yeeun0702 <kimyeeun0702@naver.com>
2025-04-01 20:32:14 +0900

    ✨ [feat] #9 유저 생성 컨트롤러 구현

---
+++ src/main/java/com/example/schedulemanageapp/domain/users/controller/UserController.java
@@ -0,0 +1,30 @@
+package com.example.schedulemanageapp.domain.users.controller;
+
+import com.example.schedulemanageapp.common.exception.code.enums.SuccessCode;
+import com.example.schedulemanageapp.common.response.ApiResponseDto;
+import com.example.schedulemanageapp.domain.users.dto.request.UserCreateRequestDto;
+import com.example.schedulemanageapp.domain.users.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping
+    public ResponseEntity<ApiResponseDto<Void>> createUser(@RequestBody @Valid final UserCreateRequestDto userCreateRequestDto) {
+        userService.createUser(userCreateRequestDto);
+        return ResponseEntity.status(201)
+                .body(ApiResponseDto.success(SuccessCode.USER_CREATE_SUCCESS, "/api/users"));
+    }
+
+
+}

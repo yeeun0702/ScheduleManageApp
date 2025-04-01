313b629358fa9a641b2344532b2e56d7ed485c42
yeeun0702 <kimyeeun0702@naver.com>
2025-04-01 20:31:48 +0900

    ✨ [feat] #9 유저 생성 비즈니스 로직 구현

---
+++ src/main/java/com/example/schedulemanageapp/domain/users/service/UserService.java
@@ -0,0 +1,23 @@
+package com.example.schedulemanageapp.domain.users.service;
+
+import com.example.schedulemanageapp.domain.users.dto.request.UserCreateRequestDto;
+import com.example.schedulemanageapp.domain.users.entity.Users;
+import com.example.schedulemanageapp.domain.users.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public void createUser(final UserCreateRequestDto userCreateRequestDto){
+        Users user = new Users(
+                userCreateRequestDto.userName(),
+                userCreateRequestDto.email(),
+                userCreateRequestDto.password()
+        );
+        userRepository.save(user);
+    }
+}

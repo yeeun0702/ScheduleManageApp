3870c6381f5e2cd84ef8a4c6829face961300c0d
yeeun0702 <kimyeeun0702@naver.com>
2025-04-01 16:35:19 +0900

    ✨ [feat] #5 일정 전체 조회 JPQL 쿼리 추가

--- src/main/java/com/example/schedulemanageapp/domain/schedule/repository/ScheduleRepository.java
+++ src/main/java/com/example/schedulemanageapp/domain/schedule/repository/ScheduleRepository.java
@@ -2,8 +2,26 @@ package com.example.schedulemanageapp.domain.schedule.repository;
 
 import com.example.schedulemanageapp.domain.schedule.entity.Schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Repository
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
+
+    /* - userId가 null이 아니면 해당 유저의 일정만 필터링
+       - updatedDate가 null이 아니면 수정일 기준 이후의 일정만 필터링
+     */
+    @Query("""
+        SELECT s FROM Schedule s
+        WHERE (:userId IS NULL OR s.users.id = :userId)
+          AND (:updatedDate IS NULL OR s.updatedAt >= :updatedDate)
+        ORDER BY s.updatedAt DESC
+    """)
+    List<Schedule> findSchedulesByConditions(
+        Long userId,
+        LocalDateTime updatedDate
+    );
 }

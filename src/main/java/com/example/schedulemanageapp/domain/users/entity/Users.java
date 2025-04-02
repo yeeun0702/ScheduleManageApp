214fad395ee0656d85988a6ade42306d43c75397
yeeun0702 <kimyeeun0702@naver.com>
2025-04-02 13:07:35 +0900

    ♻️ [refactor] #9 Users 객체 생성 방식 빌더 패턴으로 변경

--- src/main/java/com/example/schedulemanageapp/domain/users/entity/Users.java
+++ src/main/java/com/example/schedulemanageapp/domain/users/entity/Users.java
@@ -3,6 +3,7 @@ package com.example.schedulemanageapp.domain.users.entity;
 import com.example.schedulemanageapp.common.entity.BaseTimeEntity;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -25,6 +26,7 @@ public class Users extends BaseTimeEntity {
     @Column(name="password", nullable = false)
     private String password;
 
+    @Builder
     public Users(String userName, String email, String password){
         this.userName = userName;
         this.email = email;

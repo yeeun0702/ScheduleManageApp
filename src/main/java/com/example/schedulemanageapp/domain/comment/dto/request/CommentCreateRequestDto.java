b7731dc1db50eefcb1f45446e34a953bb294f4e9
yeeun0702 <kimyeeun0702@naver.com>
2025-04-03 21:32:35 +0900

    ✨ [feat] #15 CommentRepository 추가 및 댓글 요청 DTO 구현

---
+++ src/main/java/com/example/schedulemanageapp/domain/comment/dto/request/CommentCreateRequestDto.java
@@ -0,0 +1,13 @@
+package com.example.schedulemanageapp.domain.comment.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record CommentCreateRequestDto(
+        Long scheduleId,
+        Long userId,
+
+        @NotBlank @Size(max = 20, message = "최대 20글자까지 가능합니다.")
+        String commentContent
+) {
+}

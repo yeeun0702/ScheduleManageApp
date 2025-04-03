7352169706850dd03114954814ef1e339eaf92a9
yeeun0702 <kimyeeun0702@naver.com>
2025-04-03 21:32:35 +0900

    ✨ [feat] #15 CommentRepository 추가 및 댓글 요청 DTO 구현

---
+++ src/main/java/com/example/schedulemanageapp/domain/comment/repository/CommentRepository.java
@@ -0,0 +1,9 @@
+package com.example.schedulemanageapp.domain.comment.repository;
+
+import com.example.schedulemanageapp.domain.comment.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+}

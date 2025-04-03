cabeb24eacf2d713dd6c0a07a72daef1e14639b5
yeeun0702 <kimyeeun0702@naver.com>
2025-04-03 18:38:16 +0900

    ✨ [feat] #13 로그인 성공, 실패 코드 구현

--- src/main/java/com/example/schedulemanageapp/common/exception/code/enums/SuccessCode.java
+++ src/main/java/com/example/schedulemanageapp/common/exception/code/enums/SuccessCode.java
@@ -20,7 +20,8 @@ public enum SuccessCode {
     USER_READ_SUCCESS("SU2000", HttpStatus.OK, "유저를 성공적으로 조회했습니다."),
     USER_LIST_SUCCESS("SU2001", HttpStatus.OK, "유저 목록을 성공적으로 조회했습니다."),
     USER_UPDATE_SUCCESS("SU2002", HttpStatus.OK, "유저를 성공적으로 수정했습니다."),
-    USER_DELETE_SUCCESS("SU2003", HttpStatus.OK, "유저를 성공적으로 삭제했습니다.");
+    USER_DELETE_SUCCESS("SU2003", HttpStatus.OK, "유저를 성공적으로 삭제했습니다."),
+    USER_LOGIN_SUCCESS("SU2004", HttpStatus.OK, "로그인에 성공했습니다.");
 
     private final String code;
     private final HttpStatus httpStatus;

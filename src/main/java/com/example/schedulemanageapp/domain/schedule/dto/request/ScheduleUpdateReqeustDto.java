9de07ca432d10a9b605a3e376518f187337bd3ec
yeeun0702 <kimyeeun0702@naver.com>
2025-04-04 14:04:33 +0900

    ♻️ [refactor] #13 예외처리 메세지 개선

--- src/main/java/com/example/schedulemanageapp/domain/schedule/dto/request/ScheduleUpdateReqeustDto.java
+++ src/main/java/com/example/schedulemanageapp/domain/schedule/dto/request/ScheduleUpdateReqeustDto.java
@@ -5,10 +5,10 @@ import jakarta.validation.constraints.Size;
 
 public record ScheduleUpdateReqeustDto(
 
-        @NotBlank(message = "할 일은 제목은 공백일 수 없습니다.") @Size(max = 10, message = "최대 10자까지 가능합니다.")
+        @NotBlank(message = "일정의 제목은 공백일 수 없습니다.") @Size(max = 10, message = "최대 10자까지 가능합니다.")
         String todoTitle,
 
-        @NotBlank(message = "할 일은 내용은 공백일 수 없습니다.") @Size(max = 200, message = "최대 200자까지 가능합니다.")
+        @NotBlank(message = "일정의 내용은 공백일 수 없습니다.") @Size(max = 200, message = "최대 200자까지 가능합니다.")
         String todoContent,
 
         @NotBlank(message = "비밀번호는 공백일 수 없습니다.")

855f425716d48a12ea7c1abc4c505da665185935
yeeun0702 <kimyeeun0702@naver.com>
2025-04-08 23:08:45 +0900

    ♻️ [refactor] #19 BaseTimeEntity 생성, 수정 시간에  @Column(nullable = false) 설정 추가

--- src/main/java/com/example/schedulemanageapp/common/entity/BaseTimeEntity.java
+++ src/main/java/com/example/schedulemanageapp/common/entity/BaseTimeEntity.java
@@ -15,10 +15,10 @@ import java.time.LocalDateTime;
 @EntityListeners(AuditingEntityListener.class)
 public class BaseTimeEntity {
     @CreatedDate
-    @Column(updatable = false)
-//    @Temporal(TemporalType.TIMESTAMP) 생략가능
+    @Column(nullable = false, updatable = false)
     private LocalDateTime createdAt;
 
     @LastModifiedDate
+    @Column(nullable = false)
     private LocalDateTime updatedAt;
 }
